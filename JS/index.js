43bf5d7cc4041e6f65185c50074bc96e9dd0aa59
Neha Pednekar <155717961+Neha-9321@users.noreply.github.com>
2024-09-13 17:38:13 +0530

    Deleted JS directory

--- JS/index.js
+++
@@ -1,7 +0,0 @@
-function addToCart(image, name, brand, price) {
-  let cart = JSON.parse(localStorage.getItem('cart')) || [];
-  cart.push({ image, name, brand, price });
-  localStorage.setItem('cart', JSON.stringify(cart));
-  alert(`${name} has been added to the cart!`);
-  console.log('Cart after adding item:', cart); // Debugging statement
-}
